b5ebd3d0436532552533ada8cc0aa9a45f62bf19
Millena Cupolillo <millenacupolillo@gmail.com>
2024-12-10 09:50:16 -0300

    update - "salvando" connect

--- amigos_connect/app.py
+++ amigos_connect/app.py
@@ -1,16 +1,18 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 from datetime import datetime
 from conexao import conexao_fechar, conexao_abrir
-from arquivo import obter_usuario, salvar_usuario
+from arquivo import obter_usuario, salvar_usuario, salvar_connect
 import sys
 import os
 
 # Adiciona o diretório pai de 'amigos_connect' ao sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from model.Usuario import Usuario
+from model.Connect import Connect
+
 
 app = Flask("Amigos_Connect")
-app.secret_key = "uma_chave_secreta"  # Defina uma chave secreta para sessões
+app.secret_key = "uma_chave_secreta" 
 con = conexao_abrir("localhost", "root", "Millena03*", "amigosconnect")
 
 @app.route("/")
@@ -27,11 +29,7 @@ def cadastro():
 
 @app.route("/criar-connect.html")
 def cria_connect(): 
-    return render_template("/criar-connect.html")
-
-@app.route("/criar-connect.html", methods = ['POST'])
-def criar_connect(): 
-    return render_template("/criar-connect.html")
+    return render_template("/criar-connect.html", usuario = session['usuario'], usuarios = obter_usuario(con))
 
 @app.route("/info-connect.html")
 def info_connect(): 
@@ -39,11 +37,11 @@ def info_connect():
 
 @app.route("/participantes.html")
 def mostrar_participantes(): 
-    return render_template("participantes.html")
+    return render_template("participantes.html", usuario = session['usuario'])
 
 @app.route("/chat.html")
 def mostrar_chat(): 
-    return render_template("chat.html")
+    return render_template("chat.html", usuario = session['usuario'])
 
 @app.route("/feed.html")
 def feed():
@@ -63,6 +61,7 @@ def termos():
 @app.route("/cadastro.html", methods=['POST'])
 def cadastrar():
     nome = request.form['nome'].strip()
+    print(nome)
     usuario = request.form['usuario'].strip()
     email = request.form['email'].strip()
     senha = request.form['senha'].strip()
@@ -73,8 +72,7 @@ def cadastrar():
         salvar_usuario(con, usuario)
         return render_template("login.html")
     else: 
-        # Mensagem de erro caso as senhas não sejam iguais
-        return render_template("cadastro.html", erro="As senhas não coincidem!")
+        return render_template("cadastro.html")
 
 @app.route("/login.html", methods=['POST'])
 def entrar():
@@ -87,7 +85,7 @@ def entrar():
         if (usuario == u['email'] or usuario == u['nomeUsuario']) and (senha == u['senha']):
             contem = True
             session['usuario'] = {
-                'id': u['idUsuario'],               
+                'idUsuario': u['idUsuario'],               
                 'email': u['email'],         
                 'nomeUsuario': u['nomeUsuario'],  
                 'nome': u['nome'],            
@@ -98,8 +96,19 @@ def entrar():
     if contem:
         return render_template("feed.html", usuario = session['usuario'])
     else: 
-        return render_template("login.html", erro="Usuário ou senha incorretos!")
+        return render_template("login.html")
+
+@app.route("/criar-connect.html", methods = ['POST'])
+def criar_connect(): 
+    nome_connect = request.form['nome-connect'].strip()
+    print(nome_connect)
+    foto = request.files['foto-connect']
+    foto_conteudo = foto.read()
+    #amigos_select = request.form.getlist('amigos_select')
+    connect = Connect(None,nome_connect,foto_conteudo,None)
+    salvar_connect(con,connect)
+    return render_template("/chat.html", usuario = session['usuario'])
 
 conexao_fechar(con)
 
-app.run()
+app.run(debug=True)
